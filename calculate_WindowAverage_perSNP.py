5295f9d68736ac02fc5f3ece43dadd5bf4e98e6f
Dmytro Kryvokhyzha <dmytro.kryvokhyzha@evobio.eu>
2020-10-29 20:58:05 +0100

    add multiple column support calculate_WindowAverage_perSNP.py

--- calculate_WindowAverage_perSNP.py
+++ calculate_WindowAverage_perSNP.py
@@ -4,40 +4,39 @@ Calculates average values for sliding windows centred at each SNP.
 
 #Example input:
 
-CHROM  POS  stats
-chr1	14670	0.68568
-chr1	19796	0.832316
-chr1	29099	0.895923
-chr1	30259	0.80235
-chr1	31671	0.437556999117073
-chr1	41386	0.555906
-chr1	50901	0.566755
-chr2	947	0.528296
-chr2	1017	0.494082466499707
-chr2	51111	0.360812988529893
-chr2	75365	0.665836
-chr2	80686	0.500991
-chr2	173083	0.753236
-chr2	173098	0.767467
+CHROM	POS	stats1	stats2
+chr1	14670	0.69	1.7
+chr1	19796	0.83	1.8
+chr1	29099	0.90	1.9
+chr1	30259	0.80	1.8
+chr1	31671	0.44	1.4
+chr1	41386	0.56	1.6
+chr1	50901	0.57	1.6
+chr2	947	0.53	1.5
+chr2	1017	0.49	1.5
+chr2	51111	0.36	1.4
+chr2	75365	0.67	1.7
+chr2	80686	0.50	1.5
+chr2	173083	0.75	1.8
+chr2	173098	0.77	1.8
 
 #Example output:
 
-CHROM  POS  stats
-chr1	14670	0.730765199823
-chr1	19796	0.701621999853
-chr1	29099	0.682355285588
-chr1	30259	0.682355285588
-chr1	31671	0.682355285588
-chr1	41386	0.68180116652
-chr1	50901	0.651698199823
-chr2	947	0.51118923325
-chr2	1017	0.51118923325
-chr2	51111	0.513324494265
-chr2	75365	0.50921332951
-chr2	80686	0.5834135
-chr2	173083	0.7603515
-chr2	173098	0.7603515
-
+CHROM	POS	stats1	stats2
+chr1	14670	0.732	1.72
+chr1	19796	0.703333333333	1.7
+chr1	29099	0.684285714286	1.68571428571
+chr1	30259	0.684285714286	1.68571428571
+chr1	31671	0.684285714286	1.68571428571
+chr1	41386	0.683333333333	1.68333333333
+chr1	50901	0.654	1.66
+chr2	947	0.51	1.5
+chr2	1017	0.51	1.5
+chr2	51111	0.515	1.55
+chr2	75365	0.51	1.53333333333
+chr2	80686	0.585	1.6
+chr2	173083	0.76	1.8
+chr2	173098	0.76	1.8
 
 #command:
 
@@ -68,7 +67,7 @@ parser.add_argument(
     '-w', '--window', help='sliding window size', type=int, required=True)
 parser.add_argument(
     '-m', '--min', help='minimum number of SNPs to keep window',
-    type=int, required=True, default=1)
+    type=int, required=False, default=1)
 args = parser.parse_args()
 
 ############################# functions #############################
@@ -91,9 +90,11 @@ def slideWindow():
         windowEnd = i+windSize
         endIndex = bisect.bisect(posList, windowEnd)
 
-        windowAverage = average(statsList[startIndex:endIndex], minSNPs)
-
-        outputFile.write("%s\t%s\t%s\n" % (ChrPrevious, i, windowAverage))
+        outputFile.write("%s\t%s" % (ChrPrevious, i))
+        for n in statName:
+            windowAverage = average(statsDic[n][startIndex:endIndex], minSNPs)
+            outputFile.write("\t%s" % windowAverage)
+        outputFile.write("\n")
 
 ############################# program #############################
 
@@ -105,6 +106,7 @@ minSNPs = args.min
 
 with open(args.input) as datafile:
     header_line = datafile.readline()
+    statName = header_line.split()[2:]
 
     # make output header
     outputFile = open(args.output, 'w')
@@ -119,23 +121,26 @@ with open(args.input) as datafile:
         words = line.split()
         Chr = words[0]
         pos = int(float(words[1]))
-        stats = float(words[2])
+        stats = [float(i) for i in words[2:]]
         
         # read in one chromosome
         if ChrPrevious == '':
             posList = []
-            statsList = []
+            statsDic = {key: [] for key in statName}
             posList.append(pos)
-            statsList.append(stats)
+            for n in statName:
+                statsDic[n].append(stats[statName.index(n)])
         elif Chr == ChrPrevious:
             posList.append(pos)
-            statsList.append(stats)
+            for n in statName:
+                statsDic[n].append(stats[statName.index(n)])
         else:
             slideWindow()
             posList = []
-            statsList = []
+            statsDic = {key: [] for key in statName}
             posList.append(pos)
-            statsList.append(stats)
+            for n in statName:
+                statsDic[n].append(stats[statName.index(n)])
            
         ChrPrevious = Chr
         # track progress
